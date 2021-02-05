cd7bbd1192e95b765602365b5194db457e55246b
orgojy <giveawesome@gmail.com>
2021-02-05 19:41:05 +0900

    feat: receiving method in case of SqsMessageDeletionPolicy

--- src/main/java/com/example/awssqs/SqsMessageReceiver.java
+++ src/main/java/com/example/awssqs/SqsMessageReceiver.java
@@ -16,4 +16,39 @@ public class SqsMessageReceiver {
     public void receiveForQueueName(String message, @Header("SenderId") String senderId) {
         System.out.println(String.format("%s %s", message, senderId));
     }
+
+    /**
+     * When we receive message, The message is immediately deleted.
+     */
+    @SqsListener(value = "z-standard-queue", deletionPolicy = SqsMessageDeletionPolicy.NO_REDRIVE)
+    public void receiveWithNoRedrive(String message, @Header("SenderId") String senderId) {
+        System.out.println(String.format("%s %s", message, senderId));
+    }
+
+    /**
+     * Even if we receive message, we cannot delete it.
+     * NEVER <-> ALWAYS
+     */
+    @SqsListener(value = "z-standard-queue", deletionPolicy = SqsMessageDeletionPolicy.NEVER)
+    public void receiveWithNever(String message, @Header("SenderId") String senderId) {
+        System.out.println(String.format("%s %s", message, senderId));
+    }
+
+    /**
+     * Always deletes message in case of success (no exception thrown) or failure (exception thrown).
+     * ALWAYS <-> NEVER
+     */
+    @SqsListener(value = "z-standard-queue", deletionPolicy = SqsMessageDeletionPolicy.ALWAYS)
+    public void receiveWithAlways(String message, @Header("SenderId") String senderId) {
+        throw new RuntimeException("Even if an exception occurs, message is unconditionally deleted.");
+    }
+
+    /**
+     * If an exception is thrown by the listener method, the message will not be deleted.
+     * NO_REDRIVE <-> ON_SUCCESS
+     */
+    @SqsListener(value = "z-standard-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void receiveWithOnSuccess(String message, @Header("SenderId") String senderId) {
+        throw new RuntimeException("If it is thrown exception, message is not deleted.");
+    }
 }
