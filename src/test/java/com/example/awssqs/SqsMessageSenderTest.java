6a27f5fd1da7961f3a24f14bd81b3217014e7bc8
orgojy <giveawesome@gmail.com>
2021-02-16 11:46:25 +0900

    test: SQS message to send converted message

--- src/test/java/com/example/awssqs/SqsMessageSenderTest.java
+++ src/test/java/com/example/awssqs/SqsMessageSenderTest.java
@@ -2,6 +2,7 @@ package com.example.awssqs;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,5 +18,12 @@ class SqsMessageSenderTest {
         jsonObject.put("name", "Ryan An");
         jsonObject.put("age", 30);
         sqsMessageSender.send(jsonObject.toString());
+    }
+
+    @Test
+    void convertAndSend() {
+        int testId = 1;
+        SqsMessage message = SqsMessage.of(testId, "value");
+        sqsMessageSender.convertAndSend(message);
     }
 }
